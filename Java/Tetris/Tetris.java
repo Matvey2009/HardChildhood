d0a3d9efbb772d36b267017f8b2a9296486fbc79
Matvey2009 <matvey2009ua@gmail.com>
2019-10-27 21:35:51 +0300

    Update Tetris.java

--- Java/Tetris/Tetris.java
+++ Java/Tetris/Tetris.java
@@ -1,18 +1,51 @@
-import javax.swing.JFrame;
+/*27.10.2019
+ * Matvey
+ * Tetris
+ */
 
-public class Tetris {
+import java.awt.Color;                                                                                                                
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tetris extends JPanel {
 	
-	protected static int block = 30;
+	//Переменные
+	protected static int block = 30, x = 10;
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		
+		//Окно Приложения
 		JFrame jFrame = new JFrame ("Tetris");
 		jFrame.setDefaultLookAndFeelDecorated(true);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.pack();
 		jFrame.setSize (block*10, block*20);
 		jFrame.setResizable(false);
 		jFrame.setLocationRelativeTo(null);
-		jFrame.setVisible(true);		
+		jFrame.setVisible(true);
+		Tetris tetris = new Tetris();
+		jFrame.add(tetris);
 		
+		while (true) {
+			tetris.game();
+			tetris.repaint();
+			try {
+				Thread.sleep(50);
+			} catch (Exception e) {}
+		}
+	}
+	
+	// Логический Блок
+	private void game() {
+		x++;
+	}
+	
+	//Графика
+	public void paint(Graphics ctx) {
+		super.paint(ctx);
+		setBackground(Color.black);
+		
+		ctx.setColor(Color.green);
+		ctx.fillRect(50, x, 50, 50);
 	}
 }
