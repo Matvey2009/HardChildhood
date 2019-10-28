a1fda9017dd0b47f86f68e29dadf8db74757ba6c
Matvey2009 <matvey2009ua@gmail.com>
2019-10-28 21:17:04 +0300

    Update Tetris.java

--- Java/Tetris/Tetris.java
+++ Java/Tetris/Tetris.java
@@ -1,17 +1,20 @@
-/*27.10.2019
+/* 27.10.2019
  * Matvey
  * Tetris
  */
-
+ 
 import java.awt.Color;                                                                                                                
 import java.awt.Graphics;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Tetris extends JPanel {
 	
 	//Переменные
-	protected static int block = 30, x = 10;
+	protected static int block = 30, x = 0, y = 0;
 	
 	public static void main(String[] args) {
 		
@@ -26,18 +29,30 @@ public class Tetris extends JPanel {
 		Tetris tetris = new Tetris();
 		jFrame.add(tetris);
 		
+		//Проверка
+		jFrame.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent event) {
+				switch(event.getKeyCode()) {
+					case 37: x--; break; //Лево
+					case 38: y--; break; //Низ
+					case 39: x++; break; //Право
+					case 40: y++; break; //Верх
+				}
+			}
+		});
+		
 		while (true) {
 			tetris.game();
 			tetris.repaint();
 			try {
-				Thread.sleep(50);
+				Thread.sleep(500);
 			} catch (Exception e) {}
 		}
 	}
 	
 	// Логический Блок
 	private void game() {
-		x++;
+		//y++;
 	}
 	
 	//Графика
@@ -46,6 +61,6 @@ public class Tetris extends JPanel {
 		setBackground(Color.black);
 		
 		ctx.setColor(Color.green);
-		ctx.fillRect(50, x, 50, 50);
+		ctx.fillRect(x*block, y*block, 30, 30);
 	}
 }
