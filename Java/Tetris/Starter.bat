fec0a7806de45ce66da20759b1037a86b6a2926c
Matvey2009 <matvey2009ua@gmail.com>
2019-11-02 21:22:58 +0300

    Update Starter.bat

--- Java/Tetris/Starter.bat
+++ Java/Tetris/Starter.bat
@@ -1,13 +1,13 @@
 @echo off
-chcp 1251 > nul
-title Компулятор 'Java' Кода
-mode con cols=48 lines=32
+chcp 1251 >nul
+title Java
+mode con cols=48 lines=16
 color 0a
 
 echo %TIME% - Compilation
 "C:\Program Files\Java\jdk-13.0.1\bin\javac.exe" Tetris.java
 
->nul pathping /h 1 /p 3000 /q 1 /w 1 127.0.0.1
+>nul pathping /h 1 /p 100 /q 1 /w 1 127.0.0.1
 
 echo %TIME% - Start
 "C:\Program Files\Java\jdk-13.0.1\bin\java.exe" Tetris
