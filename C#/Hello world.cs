ba88fd5fdb0e539191e1e6facebe9e437c36fff8
Matvey2009 <matvey2009ua@gmail.com>
2020-02-01 21:05:18 +0300

    Create Hello world.cs

---
+++ C#/Hello world.cs
@@ -0,0 +1,22 @@
+/*
+ *Уроки по c#
+ *Матвей пркидько сергеевич.
+ * 02.01.2020
+ *Привет я Матвей ,a ты Максим-дурачёк.Понятно пока!
+ */
+
+using System;
+
+namespace MyProgram
+{
+	class hw
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world");
+			Console.WriteLine(DateTime.Now);
+			Console.ReadKey();
+		}
+		
+	}
+}
