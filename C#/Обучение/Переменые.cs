92d97be00e40a3411b4d123048572f4d1d9fbc48
Matvey2009 <matvey2009ua@gmail.com>
2020-02-05 18:14:10 +0300

    Create Переменые.cs

---
+++ C#/Обучение/Переменые.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Обучение
+{
+    class Переменые
+    {
+        public void консоль()
+        {
+            Console.Write("Сравнение 2_>_3 - ");
+            Console.WriteLine(2 > 3);
+            Console.WriteLine();
+
+            Console.Write("Сравнение 2_>_3 - ");
+            bool x = 2 > 3;
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+            Console.Write("Сравнение 2_<_3 - ");
+            x = 2 < 3;
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+            Console.Write("Сравнение 2_=_3 - ");
+            x = 2 == 3;
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+        }
+    }
+}
