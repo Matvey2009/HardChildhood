23927921bca8c2f1464585624b4e8abdb58ed38f
Matvey2009 <matvey2009ua@gmail.com>
2020-02-05 18:14:13 +0300

    Create Урок.cs

---
+++ C#/Обучение/Урок.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Обучение
+{
+    class Урок
+    {
+        Переменые переменые = new Переменые();
+
+        static void Main(string[] args)
+        {
+            Урок урок = new Урок();
+            урок.Выводнаконсоль();
+            Console.ReadKey();
+        }
+        void Выводнаконсоль()
+        {
+            переменые.консоль();
+        }
+
+    }
+}
