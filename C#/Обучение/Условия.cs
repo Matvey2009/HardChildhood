5ae839e2f04dfe7c0b3a7eebd58de32c4bbc29cb
Matvey2009 <matvey2009ua@gmail.com>
2020-02-08 16:37:25 +0300

    Create Условия.cs

---
+++ C#/Обучение/Условия.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Обучение2
+{
+    class Условия
+        
+    {
+        public void Консоль2()
+        {
+            bool x = 2 < 3;
+            if (x)
+            {
+                Console.WriteLine($"Два больше трёх 2_>_3 - {0}\n");
+            }
+            else
+            {
+                Console.WriteLine($"Правда{0}\n");
+            }
+        }
+    }
+}
