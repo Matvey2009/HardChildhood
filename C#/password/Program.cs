4798a49ebf68d92a59c66898bab64921d1346271
Matvey2009 <matvey2009ua@gmail.com>
2020-03-08 15:18:45 +0300

    Create Program.cs

---
+++ C#/password/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Переменые
+            string password = "1234", _password = "";
+
+            //Провекрка пороля
+            while (password != _password)
+            {
+                Console.WriteLine("Введите пароль: ");
+                _password = Console.ReadLine();
+                if (_password != password) Console.WriteLine("Доступ закрыт");
+            }
+
+            //Старт
+            Console.WriteLine("Доступ открыт");
+            Console.ReadKey();
+        }
+    }
+}
