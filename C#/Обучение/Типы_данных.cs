ca56034d68697285cf7664106aff902df69e3a8c
Matvey2009 <matvey2009ua@gmail.com>
2020-03-15 16:46:14 +0300

    Update Типы_данных.cs

--- C#/Обучение/Типы_данных.cs
+++ C#/Обучение/Типы_данных.cs
@@ -12,18 +12,18 @@ namespace Обучение
         {
             Console.WriteLine($"bool(8bit) - {true}\n");
             Console.WriteLine($"sbyte(8bit) - {-123}\n");
-            Console.WriteLine($"byte(8bit) - {250}\n");
+            Console.WriteLine($"byte(8b it) - {250}\n");
             Console.WriteLine($"short(16bit) - {-32000}\n");
             Console.WriteLine($"unshort(16bit) - {60000}\n");
             Console.WriteLine($"int(32bit) - {-123456}\n");
             Console.WriteLine($"uint(32bit) - {9876543210}\n");
             Console.WriteLine($"long(64bit) - {-64000000000}\n");
             Console.WriteLine($"unlong(64bit) - {12800000000}\n");
-            Console.WriteLine($"float(---) - {123.456f}\n");
-            Console.WriteLine($"double(---) - {123.4567890f}\n");
-            Console.WriteLine($"decimal(---) - {123456.789}\n");
-            Console.WriteLine($"char(---) - {"x"}\n");
-            Console.WriteLine($"unlong(---) - {"hello"}\n");
+            Console.WriteLine($"float(32bit) - {123.456f}\n");
+            Console.WriteLine($"double(64bit) - {123.4567890f}\n");
+            Console.WriteLine($"decimal(128bit) - {123456.789}\n");
+            Console.WriteLine($"char(16bit) - {"x"}\n");
+            Console.WriteLine($"string(ссылка) - {"hello"}\n");
         }
     }
 }
