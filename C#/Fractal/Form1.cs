d36d6f6289d49cd338ba6fb0b28311ac9eeec0ad
Matvey2009 <matvey2009ua@gmail.com>
2020-03-23 21:09:45 +0300

    Create Form1.cs

---
+++ C#/Fractal/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        //Переменые
+        private Bitmap bitmap;
+        private Point point;
+        private Point[] angle;
+        private Random random;
+        private int number, rand;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
