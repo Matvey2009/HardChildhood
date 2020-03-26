ef6c2f0b1dc2bc87180d6110d09d14a3da98e977
Matvey2009 <matvey2009ua@gmail.com>
2020-03-26 21:17:02 +0300

    Update Урок.cs

--- C#/Обучение/Урок.cs
+++ C#/Обучение/Урок.cs
@@ -14,7 +14,7 @@ namespace Обучение
         public string name;
         public void Вывод()
         {
-            Console.WriteLine(name);
+            Console.WriteLine(name + " " + x + " " + y);
         }
     }
 
@@ -29,8 +29,10 @@ namespace Обучение
             Урок урок = new Урок();
             урок.Выводнаконсоль();
             Console.ReadKey();
-            
+
         }
+
+        unsafe
         void Выводнаконсоль()
         {
             //переменые.консоль();
@@ -42,10 +44,32 @@ namespace Обучение
             Строка iks = "Дирежабыль";
             КвадраМатвеюс.WriteLine(durak + " " + dabil + " " + iks);
 
+            //Объект структуры
+            Точка тчк = new Точка();
+            тчк.x = 1_234_567;
+            тчк.y = 999_876_543;
+            тчк.name = "Матвей";
+            тчк.Вывод();
+
+            //Указатели\\
+            int k = 125;
+            int* Указатель = &k;
+            Console.WriteLine("Регистор памяти " + (long)Указатель);
+
+            //Динамичекский тип данных
+            dynamic d = 1;
+            Console.WriteLine(d);
+            d = "Матвей";
+            Console.WriteLine(d);
+
+            //Округлегие
+            Console.WriteLine(Math.Round(Math.PI, 15));
+            Console.WriteLine(Math.Abs(-123));
+            Console.WriteLine(Math.Pow(10,10));
+            Console.WriteLine(Math.Sqrt(16));
+            Console.WriteLine(Math.Min(10, 15));
+            Console.WriteLine(Math.Sin(30));
+            Console.WriteLine(Math.Cos(30));
         }
-
-        Точка точка = new Точка();
-
     }
-
 }
