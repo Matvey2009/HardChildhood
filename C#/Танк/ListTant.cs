d9a2eb5cf4aebe18e827a024c0124bdc66ca1a27
Matvey2009 <matvey2009ua@gmail.com>
2020-04-01 21:21:58 +0300

    Create ListTant.cs

---
+++ C#/Танк/ListTant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Танк
+{
+    class ListTant
+    {
+        Tank tank;
+        
+        public void DriweListTant(Graphics g)
+        {
+            for (byte i = 0; i < 10; i++)
+            {
+                tank = new Tank();
+                tank.DrawTank(g);
+            }
+        }
+    }
+}
