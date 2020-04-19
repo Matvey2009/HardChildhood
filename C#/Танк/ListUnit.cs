191ce4808567f7c45259ffeea92cea46d89770a6
Matvey2009 <matvey2009ua@gmail.com>
2020-04-19 11:52:51 +0300

    Create ListUnit.cs

---
+++ C#/Танк/ListUnit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Танк
+{
+    class ListUnit
+    {
+        private Random random = new Random();
+        private List<object> listUnits = new List<object>();
+        private byte count = 10;
+
+        //Создаём лист танков
+        public List<object> CreateListUnit()
+        {
+            for (byte i = 1; i <= count; i++) 
+            {
+                listUnits.Add(new Tank
+                {
+                    id = i,
+                    position = StartPosition()
+                });
+
+                listUnits.Add(new Car
+                {
+                    id = i,
+                    position = StartPosition()
+                });
+            }
+            return listUnits;
+        }
+
+        //Отрисовка лист танков
+        public void DriweListUnit(Graphics g, Point cursor)
+        {
+            foreach (dynamic unit in listUnits)
+            {
+                unit.DrawUnit(g, cursor);
+            }
+        }
+
+        //Стартовая позиция
+        private Point StartPosition()
+        {
+            Point position = new Point();
+            position.X = random.Next(1280);
+            position.Y = random.Next(720);
+            return position;
+        }
+    }
+}
