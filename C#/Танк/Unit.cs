0c911ec8947d0bb057f4b764b7b95363b8e06205
Matvey2009 <matvey2009ua@gmail.com>
2020-04-20 15:23:11 +0300

    Update Unit.cs

--- C#/Танк/Unit.cs
+++ C#/Танк/Unit.cs
@@ -5,12 +5,31 @@ namespace Танк
 {
     abstract class Unit
     {
-        public int id;//Нопер Unit
+        private static uint ID;
+
+        public uint id = ++ID;//Нопер Unit
         public PointF position;//Местоподожение
 
         public PointF target;//Цель
         public float speed = 1;
         public float vector; //Угол повророта корпуса
+
+        private Font font = new Font("Areal", 10, FontStyle.Bold, GraphicsUnit.Point);
+        private SolidBrush color = new SolidBrush(Color.Yellow);
+        private Pen pen = new Pen(Color.Red, 3);
+
+        //Номер и полоска жизни
+        public void DrawInfo(Graphics g)
+        {
+            //Наименование
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawString(id.ToString(), font, color, -7 , -40);
+            g.ResetTransform();
+            //Полоска жизни
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawLine(pen, -20, -20, 20, -20);
+            g.ResetTransform();
+        }
 
         //Расчёт поворота unit
         public float Vector()
