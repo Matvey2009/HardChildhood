81fc869d49a7209142c2aa6b092ab3ac00089ec7
Matvey2009 <matvey2009ua@gmail.com>
2020-05-04 21:20:35 +0300

    Create Aobject.cs

---
+++ C#/Танк/Aobject.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Танк
+{
+    abstract class Aobject
+    {
+        public PointF position;//Местоподожение
+        public PointF target;//Цель
+        public float speed;
+    }
+}
