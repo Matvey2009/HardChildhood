bbff15f5bf83a3c01d8e632b738acf9c8677c1ba
Matvey2009 <matvey2009ua@gmail.com>
2020-05-04 21:20:30 +0300

    Create Shot.cs

---
+++ C#/Танк/Shot.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Танк
+{
+    class Shot : Aobject
+    {
+        Pen pen;
+
+        //Рисунок пульки
+        public void DrawShot(Graphics g, Point cursor)
+        {
+            target = cursor;
+            pen = new Pen(Color.DarkOrange, 3);
+            g.DrawLine(pen, position, target);
+        }
+    }
+}
