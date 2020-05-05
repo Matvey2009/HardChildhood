a33b73d6c425bb8bb34521d5a68a26dd204caefb
Matvey2009 <matvey2009ua@gmail.com>
2020-05-05 21:11:28 +0300

    Update Shot.cs

--- C#/Танк/Shot.cs
+++ C#/Танк/Shot.cs
@@ -1,17 +1,41 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace Танк
 {
     class Shot : Aobject
     {
+        private PointF position0;
         Pen pen;
 
         //Рисунок пульки
-        public void DrawShot(Graphics g, Point cursor)
+        public void DrawShot(Graphics g)
         {
-            target = cursor;
+            position = Position();
             pen = new Pen(Color.DarkOrange, 3);
-            g.DrawLine(pen, position, target);
+            g.DrawLine(pen, position, position0);
+        }
+
+        //Расчёт позиции unit
+        public PointF Position()
+        {
+            vector = Vector();
+            position0 = position;
+            position.X += speed * (float)Math.Cos(vector);
+            position.Y += speed * (float)Math.Sin(vector);
+
+            return position;
+        }
+
+        //Угол на цель
+        private float Vector()
+        {
+            float catetX = target.X - position.X;
+            float catetY = target.Y - position.Y;
+            vector = (float)(Math.Atan2(catetY, catetX) * 180 / Math.PI + 90);
+            if (vector < 0) vector += 360;
+
+            return vector;
         }
     }
 }
