61997d26873653030b1b8984eddb9d7e6b7102e7
Matvey2009 <matvey2009ua@gmail.com>
2020-05-06 21:40:58 +0300

    Create Game.cs

---
+++ C#/Танк/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Танк
+{
+    class Game
+    {
+        private ListUnit RedParty, BlueParty;
+        private ListShot listShot;
+        Shot shot;
+
+        // Старт Игры
+        public void StartGame()
+        {
+            RedParty = new ListUnit();
+            RedParty.CreateListUnit();
+
+            BlueParty = new ListUnit();
+            BlueParty.CreateListUnit();
+
+            SW();
+            //Sound();
+
+            shot = new Shot();
+            shot.position = new PointF(50, 50);
+            shot.target = new PointF(1000, 600);
+            shot.speed = 20;
+        }
+
+        //Шаг Игры
+        public void StepGame(Graphics g, Point cursor) 
+        {
+            RedParty.DriweListUnit(g, cursor);
+            BlueParty.DriweListUnit(g, cursor);
+
+            shot.DrawShot(g);
+        }
+
+        //Звук заставки
+        async private void SW()
+        {
+            await Task.Run(() =>
+            {
+                Console.Beep(440, 500);
+                Console.Beep(440, 500);
+                Console.Beep(440, 500);
+                Console.Beep(349, 350);
+                Console.Beep(523, 150);
+                Console.Beep(440, 500);
+                Console.Beep(349, 350);
+                Console.Beep(523, 150);
+                Console.Beep(440, 1000);
+                Console.Beep(659, 500);
+                Console.Beep(659, 500);
+                Console.Beep(659, 500);
+                Console.Beep(698, 350);
+                Console.Beep(523, 150);
+                Console.Beep(415, 500);
+                Console.Beep(349, 350);
+                Console.Beep(523, 150);
+                Console.Beep(440, 1000);
+            });
+        }
+        private void Sound()
+        {
+            //261
+            //293
+            //329
+            //349
+            //392
+            //440
+            //493
+            Console.Beep(261, 400);
+            Console.Beep(293, 400);
+            Console.Beep(440, 400);
+            Console.Beep(293, 400);
+            Console.Beep(493, 400);
+            Console.Beep(440, 400);
+            Console.Beep(349, 400);
+            Console.Beep(293, 400);
+            Console.Beep(349, 400);
+            Console.Beep(349, 400);
+            Console.Beep(493, 400);
+            Console.Beep(440, 400);
+            Console.Beep(493, 400);
+        }
+    }
+}
