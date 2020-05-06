e4ef143cc9297505c678dca64e2c0dabefe51e1d
Matvey2009 <matvey2009ua@gmail.com>
2020-05-06 21:41:04 +0300

    Update FormTanks.cs

--- C#/Танк/FormTanks.cs
+++ C#/Танк/FormTanks.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Танк
 {
     public partial class FormTanks : Form
     {
+        private Game game;
         public Graphics g;
-        private ListUnit listUnit;
         private Point cursor;
-        Shot shot;
 
         //Окно
         public FormTanks()
@@ -26,15 +24,8 @@ namespace Танк
         //Загруска окна
         private void Form1_Load(object sender, EventArgs e)
         {
-            listUnit = new ListUnit();
-            listUnit.CreateListUnit();
-            SW();
-            //Sound();
-
-            shot = new Shot();
-            shot.position = new PointF(50, 50);
-            shot.target = new PointF(1000, 600);
-            shot.speed = 20;
+            game = new Game();
+            game.StartGame();
         }
 
         //Обновление окна
@@ -43,9 +34,7 @@ namespace Танк
             g = e.Graphics;
             g.SmoothingMode = SmoothingMode.AntiAlias;
             cursor = PointToClient(Cursor.Position);
-            listUnit.DriweListUnit(g, cursor);
-
-            shot.DrawShot(g);
+            game.StepGame(g, cursor);
         }
 
         //Таймер
@@ -59,54 +48,6 @@ namespace Танк
         {
             if (timer.Enabled == true) timer.Enabled = false;
             else timer.Enabled = true;
-        }
-        //Звук заставки
-        async private void SW()
-        {
-            await Task.Run(() =>
-            {
-                Console.Beep(440, 500);
-                Console.Beep(440, 500);
-                Console.Beep(440, 500);
-                Console.Beep(349, 350);
-                Console.Beep(523, 150);
-                Console.Beep(440, 500);
-                Console.Beep(349, 350);
-                Console.Beep(523, 150);
-                Console.Beep(440, 1000);
-                Console.Beep(659, 500);
-                Console.Beep(659, 500);
-                Console.Beep(659, 500);
-                Console.Beep(698, 350);
-                Console.Beep(523, 150);
-                Console.Beep(415, 500);
-                Console.Beep(349, 350);
-                Console.Beep(523, 150);
-                Console.Beep(440, 1000);
-            });
-        }
-         private void Sound()
-        {
-            //261
-            //293
-            //329
-            //349
-            //392
-            //440
-            //493
-            Console.Beep(261, 400);
-            Console.Beep(293, 400);
-            Console.Beep(440, 400);
-            Console.Beep(293, 400);
-            Console.Beep(493, 400);
-            Console.Beep(440, 400);
-            Console.Beep(349, 400);
-            Console.Beep(293, 400);
-            Console.Beep(349, 400);
-            Console.Beep(349, 400);
-            Console.Beep(493, 400);
-            Console.Beep(440, 400);
-            Console.Beep(493, 400);
         }
     }
 }
