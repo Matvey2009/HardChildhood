ac0736598e0c09f0d963403dcc3a2219dbf99c55
Matvey2009 <matvey2009ua@gmail.com>
2020-05-07 21:12:34 +0300

    Update Car.cs

--- C#/Танк/Car.cs
+++ C#/Танк/Car.cs
@@ -7,20 +7,25 @@ namespace Танк
         private static Size size = new Size(64, 64);
         private Bitmap bitmap = new Bitmap(Properties.Resources.car);
         private Rectangle body = new Rectangle(new Point(0, 0), size);
+        private Pen pen;
 
         //Отрисовка танка
         public void DrawUnit (Graphics g, Point cursor)
         {
-            speed = 3;
+            speed = 2;
             target = cursor;
             vector = Vector(vector, speed);
-            Position();
+            PositionUnit();
 
             //Машина
             g.TranslateTransform(position.X, position.Y);
             g.RotateTransform(vector);
             g.DrawImage(bitmap, -16, -32, body, GraphicsUnit.Pixel);
             g.ResetTransform();
+            g.ResetTransform();
+
+            pen = new Pen(color, 3);
+            g.DrawEllipse(pen, position.X - 5, position.Y - 5, 8, 8);
 
             DrawInfo(g);
         }
