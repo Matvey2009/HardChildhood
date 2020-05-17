db855a467aedbde81f42d8c87486504e5a484c67
Matvey2009 <matvey2009ua@gmail.com>
2020-05-17 11:20:44 +0300

    Create ListShot.cs

---
+++ C#/Танк/List/ListShot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Танк
+{
+    class ListShot
+    {
+        public List<Shot> listShot = new List<Shot>();
+
+        //Добавление выстрела
+        
+        public void newShot(dynamic unit)
+        {
+            listShot.Add(new Shot()
+            {
+                color = unit.color,
+                position = unit.position,
+                target = unit.target,
+                vector = (float)Math.Atan2(unit.target.Y - unit.position.Y, unit.target.X - unit.position.X)
+            });
+        }
+
+        //Отрисовка пульки
+        public void DrawListShot(Graphics g)
+        {
+            foreach (Shot shot in listShot)
+            {
+                shot.DrawShot(g);
+            }
+        }
+    }
+}
