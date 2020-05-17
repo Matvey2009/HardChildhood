64089e5491797c363de25bd5e9abd14a3af31a01
Matvey2009 <matvey2009ua@gmail.com>
2020-05-17 11:20:34 +0300

    Create Car.cs

---
+++ C#/Танк/Object/Car.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace Танк
+{
+    class Car : Unit, IDrawn
+    {
+        private static Size size = new Size(64, 64);
+        private Bitmap bitmap = new Bitmap(Properties.Resources.car);
+        private Rectangle body = new Rectangle(new Point(0, 0), size);
+        private Pen pen;
+
+        /// <summary>
+        /// Конструкор машинки
+        /// </summary>
+        public Car()
+        {
+            speed = 2;
+            live = 10;
+        }
+
+        //Отрисовка танка
+        public void DrawUnit (Graphics g, Point cursor)
+        {
+            target = cursor;
+            vector = Vector(vector, speed);
+            PositionUnit();
+
+            //Машина
+            g.TranslateTransform(position.X, position.Y);
+            g.RotateTransform(vector);
+            g.DrawImage(bitmap, -16, -32, body, GraphicsUnit.Pixel);
+            g.ResetTransform();
+            g.ResetTransform();
+
+            pen = new Pen(color, 3);
+            g.DrawEllipse(pen, position.X - 5, position.Y - 5, 8, 8);
+
+            DrawInfo(g);
+        }
+    }
+}
