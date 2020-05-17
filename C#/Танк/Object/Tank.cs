d52b81ccbe552872617f995695d34d517705b950
Matvey2009 <matvey2009ua@gmail.com>
2020-05-17 11:20:31 +0300

    Create Tank.cs

---
+++ C#/Танк/Object/Tank.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+
+namespace Танк
+{
+    class Tank : Unit, IDrawn
+    {
+        private static Size size = new Size (128, 128);
+        private float vectorTower; //Угол повророта башня
+        private readonly Bitmap bitmap = new Bitmap(Properties.Resources.Танкpng);
+        private Rectangle body = new Rectangle(new Point(0, 0), size);
+        private Rectangle tower = new Rectangle(new Point(128, 0), size);
+        private Pen pen;
+
+        /// <summary>
+        /// Конструкор танка
+        /// </summary>
+        public Tank()
+        {
+            speed = 1;
+            live = 50;
+        }
+
+        //Отрисовка танка
+        public void DrawUnit(Graphics g, Point cursor)
+        {
+            target = cursor;
+            vector = Vector(vector, speed);
+            vectorTower = Vector(vectorTower, speed*2);
+            PositionUnit();
+
+            #region *** Отрисовка по частям ***
+            //Корпус
+            g.TranslateTransform(position.X, position.Y);
+            g.RotateTransform(vector);
+            g.DrawImage(bitmap, -68, -70, body, GraphicsUnit.Pixel);
+            g.ResetTransform();
+
+            //Башня
+            g.TranslateTransform(position.X, position.Y);
+            g.RotateTransform(vectorTower);
+            g.DrawImage(bitmap, -48, -87, tower, GraphicsUnit.Pixel);
+            g.ResetTransform();
+
+            pen = new Pen(color, 3);
+            g.DrawEllipse(pen, position.X-15, position.Y-15, 32, 32);
+
+            DrawInfo(g);
+            #endregion
+        }
+    }
+}
