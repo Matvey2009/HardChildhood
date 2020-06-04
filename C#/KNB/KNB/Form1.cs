d6f83bd90d283ffef121e353a12020d26f010694
Matvey2009 <matvey2009ua@gmail.com>
2020-06-04 21:04:14 +0300

    Create Form1.cs

---
+++ C#/KNB/KNB/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace KNB
+{
+    public partial class Победы : Form
+    {
+        KNB knb = new KNB();
+
+        public Победы()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            labelplayer.Text = "Вы выбрали Камень";
+            knb.StepGame(0);
+            StepGame();
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            labelplayer.Text = "Вы выбрали Ножницы";
+            knb.StepGame(1);
+            StepGame();
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            labelplayer.Text = "Вы выбрали Бумагу";
+            knb.StepGame(2);
+            StepGame();
+        }
+        private void StepGame()
+        {
+            labelDraw.Text = knb.draw.ToString();
+            labelLose.Text = knb.lose.ToString();
+            labelWin.Text = knb.win.ToString();
+
+            labelGame.Text = knb.result;
+        }
+    }
+}
