2cef0c6007f70bef2d18678b9be65d838ff041a7
Matvey2009 <matvey2009ua@gmail.com>
2020-06-04 21:04:12 +0300

    Create Bot.cs

---
+++ C#/KNB/KNB/Bot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KNB
+{
+    class Bot
+    {
+        public byte bot;
+        Random random = new Random();
+
+        public void randbot()
+        {
+            bot = (byte)random.Next(2);
+        }
+    }
+}
