0ac535630ae61ad795bb7ed7b17ce8fe3f21fe58
Matvey2009 <matvey2009ua@gmail.com>
2020-06-11 21:23:26 +0300

    Update Game.cs

--- C#/Танк/Game.cs
+++ C#/Танк/Game.cs
@@ -7,6 +7,7 @@ namespace Танк
     {
         private List<ListUnit> ListParty;
         private ListShot listShot;
+        private Action actoin;
 
         //Старт Игры
         public void StartGame()
@@ -17,6 +18,7 @@ namespace Танк
             ListParty.Add(new ListUnit(Color.Blue, new Point(90, 20), 20));
             ListParty.Add(new ListUnit(new Point(90, 80), 10, 10));
             ListParty.Add(new ListUnit(Color.Yellow, new Point(10, 80), 10));
+            actoin = new Action();
 
             //Sound.SW();
             //Sound();
@@ -27,6 +29,8 @@ namespace Танк
         //Шаг Игры
         public void StepGame(Graphics g, Point cursor) 
         {
+            actoin.ActUnit(ListParty, listShot);
+
             foreach (ListUnit party in ListParty)
                 party.DriweListUnit(g, cursor, listShot);
 
