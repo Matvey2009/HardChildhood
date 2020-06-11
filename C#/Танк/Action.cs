4d39fe86f5e8c5f0a88cbbb93fd4f88fafb98a71
Matvey2009 <matvey2009ua@gmail.com>
2020-06-11 21:23:23 +0300

    Create Action.cs

---
+++ C#/Танк/Action.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Танк
+{
+    class Action
+    {
+        public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
+        {
+            foreach (ListUnit party in ListParty)
+                foreach (dynamic unit in party.listUnits)
+                {
+                    unit.vector = unit.Vector(unit.vector, unit.speed);
+                    unit.PositionUnit();
+                    unit.speed = 10;
+                }
+        }
+    }
+}
