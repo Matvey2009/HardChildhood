ce52956065f53d1ab97edf466841ba76fd1e2251
Matvey2009 <matvey2009ua@gmail.com>
2020-06-12 18:00:41 +0300

    Create Act.cs

---
+++ C#/Танк/Act.cs
@@ -0,0 +1,11 @@
+﻿namespace Танк
+{
+    public enum Act
+    {
+        WAIT, //Ожидание
+        FIND, //Поиск цели
+        MOVE, //Передвежение
+        FIRE, //Огонь
+        DEAD  //Смерть
+    }
+}
