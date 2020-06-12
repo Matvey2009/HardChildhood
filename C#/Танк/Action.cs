a9693aee42cfaccb4fa235264084729e5eb4250c
Matvey2009 <matvey2009ua@gmail.com>
2020-06-12 18:00:40 +0300

    Update Action.cs

--- C#/Танк/Action.cs
+++ C#/Танк/Action.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System.Net.Http.Headers;
 
 namespace Танк
 {
     class Action
     {
+        //Обход всех юнитов
         public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
         {
             foreach (ListUnit party in ListParty)
                 foreach (dynamic unit in party.listUnits)
+                    if (unit.act != Act.DEAD) Logic(unit);
+        }
+
+        //Логика действий
+        private void Logic(dynamic unit)
+        {
+            {
+                unit.vector = unit.Vector(unit.vector, unit.speed);
+                unit.PositionUnit();
+
+                switch(unit.act)
                 {
-                    unit.vector = unit.Vector(unit.vector, unit.speed);
-                    unit.PositionUnit();
-                    unit.speed = 10;
+                    case Act.WAIT:
+                        break;
+
+                    case Act.FIND:
+                        break;
+
+                    case Act.MOVE:
+                        break;
+
+                    case Act.FIRE:
+                        break;
+
+                    default:
+                        unit.act = Act.WAIT;
+                        break;
                 }
+            }
         }
     }
 }
