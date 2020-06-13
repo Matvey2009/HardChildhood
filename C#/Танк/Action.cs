359e4a5fa57b9f260095bcd8da9191f339fac565
Matvey2009 <matvey2009ua@gmail.com>
2020-06-13 21:09:07 +0300

    Update Action.cs

--- C#/Танк/Action.cs
+++ C#/Танк/Action.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Net.Http.Headers;
+using System.Windows.Forms;
+using System.Xml.XPath;
 
 namespace Танк
 {
     class Action
     {
+
+        private List<ListUnit> ListParty;
+
         //Обход всех юнитов
         public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
         {
+            this.ListParty = ListParty;
+
             foreach (ListUnit party in ListParty)
                 foreach (dynamic unit in party.listUnits)
                     if (unit.act != Act.DEAD) Logic(unit);
@@ -18,21 +25,23 @@ namespace Танк
         private void Logic(dynamic unit)
         {
             {
-                unit.vector = unit.Vector(unit.vector, unit.speed);
-                unit.PositionUnit();
-
                 switch(unit.act)
                 {
                     case Act.WAIT:
+                        goto case Act.FIRE;
+                        ActWAIT(unit);
                         break;
 
                     case Act.FIND:
+                        ActFIND(unit);
                         break;
 
                     case Act.MOVE:
+                        ActMOVE(unit);
                         break;
 
                     case Act.FIRE:
+                        ActFIRE(unit);
                         break;
 
                     default:
@@ -40,6 +49,29 @@ namespace Танк
                         break;
                 }
             }
+        }
+
+        //Процес ожидание
+        private void ActWAIT(dynamic unit)
+        {
+            if (unit.live <= 0)
+                unit.act = Act.DEAD;
+        }
+        //Процес поиска
+        private void ActFIND(dynamic unit)
+        {
+
+        }
+        //Процес зближение
+        private void ActMOVE(dynamic unit)
+        {
+
+        }
+        //Процес отаки
+        private void ActFIRE(dynamic unit)
+        {
+            unit.vector = unit.Vector(unit.vector, unit.speed);
+            unit.PositionUnit();
         }
     }
 }
