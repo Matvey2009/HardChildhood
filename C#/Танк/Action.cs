1df81f5e70364428a42bdf5144dfa413c2adb9f2
Matvey2009 <matvey2009ua@gmail.com>
2020-06-20 17:21:57 +0300

    Update Action.cs

--- C#/Танк/Action.cs
+++ C#/Танк/Action.cs
@@ -1,14 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Net.Http.Headers;
-using System.Windows.Forms;
-using System.Xml.XPath;
+using Game2D;
 
 namespace Танк
 {
     class Action
     {
-
         private List<ListUnit> ListParty;
 
         //Обход всех юнитов
@@ -54,24 +51,41 @@ namespace Танк
         //Процес ожидание
         private void ActWAIT(dynamic unit)
         {
+            //Есле танк мёртв
             if (unit.live <= 0)
                 unit.act = Act.DEAD;
+
+            //Поиск цели
+            FindTarget(unit);
+
         }
+
         //Процес поиска
         private void ActFIND(dynamic unit)
         {
 
         }
+
         //Процес зближение
         private void ActMOVE(dynamic unit)
         {
 
         }
+
         //Процес отаки
         private void ActFIRE(dynamic unit)
         {
             unit.vector = unit.Vector(unit.vector, unit.speed);
             unit.PositionUnit();
+        }
+
+        //Поиск цели
+        private float FindTarget(dynamic unit)
+        {
+            foreach (ListUnit party in ListParty)
+                foreach (dynamic findUnit in party.listUnits)
+                    Func2D.Delta(unit.position, findUnit.position);
+            return 0;
         }
     }
 }
