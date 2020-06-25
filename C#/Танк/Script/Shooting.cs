264349c940d1e640efec1233f993dcfe4a9b929f
Matvey2009 <matvey2009ua@gmail.com>
2020-06-25 20:11:15 +0300

    Create Shooting.cs

---
+++ C#/Танк/Script/Shooting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Танк.Script
+{
+    class Shooting
+    {
+        //Расчёт стрельбы
+        public void ActShot(List<ListUnit> ListParty, ListShot listShot)
+        {
+            foreach (Shot shot in listShot.listShot)
+            {
+                shot.MoveShot();
+                /*if (shot.speed < 2)
+                    listShot.RemoveShot(shot);*/
+            }
+        }
+    }
+}
