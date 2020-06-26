b6f8b0ed1a2495df75854670f64ffdccf9dde129
Matvey2009 <matvey2009ua@gmail.com>
2020-06-26 16:59:30 +0300

    Create Bang.cs

---
+++ C#/Танк/Object/Bang.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace Танк
+{
+    class Bang
+    {
+        public PointF position;
+        public byte time;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Bang(PointF position)
+        {
+            this.position = position;
+        }
+
+        public void DrawBang(Graphics g)
+        {
+            g.TranslateTransform(position.X, position.Y);
+            g.FillEllipse(new SolidBrush(Color.FromArgb(192 - time, time + 128, time, 0)),
+                new RectangleF( -time / 2, -time / 2, time, time));
+            g.ResetTransform();
+        }
+    }
+}
