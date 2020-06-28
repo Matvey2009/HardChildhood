21099a5f2655bd0c57707caa6dc5ea5e6615b50f
Matvey2009 <matvey2009ua@gmail.com>
2020-06-28 14:02:20 +0300

    Update Shooting.cs

--- C#/Танк/Script/Shooting.cs
+++ C#/Танк/Script/Shooting.cs
@@ -4,19 +4,34 @@ namespace Танк.Script
 {
     class Shooting
     {
-        private ListShot listShot;
+        private Shot shot;
+        private Bang bang;
+        private Crator crater;
 
         //Расчёт стрельбы
         public void ActShot(List<ListUnit> ListParty, ListShot listShot)
         {
-            this.listShot = listShot;
-
-            foreach (Shot shot in listShot.listShot)
+            //Расчёт пуль
+            for (int i = 0; i < listShot.listShot.Count; i++)
             {
+                shot = listShot.listShot[i];
                 shot.MoveShot();
+
                 if (shot.speed < 5)
                     listShot.RemoveShot(shot);
+         
             }
+
+            //Расчёт взрывов
+            for (int i = 0; i < listShot.listBang.Count; i++)
+            {
+                bang = listShot.listBang[i];
+                if (bang.time > 96)
+                    //_-_-_-_-_ Расчёт дамагА _-_-_-_-_\\
+                    listShot.RemoveBang(bang);
+                else
+                    bang.time += 1;
+            }    
         }
     }
 }
