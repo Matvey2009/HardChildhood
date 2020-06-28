a4f10ce54c1ed2ac391396ad3899d2a9eb43b092
Matvey2009 <matvey2009ua@gmail.com>
2020-06-28 14:02:26 +0300

    Create Crator.cs

---
+++ C#/Танк/Object/Crator.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace Танк
+{
+    class Crator
+    {
+        public PointF position;
+        public byte time;
+
+        public Crator(PointF position)
+        {
+            this.position = position;
+        }
+
+        public void DrawCrator(Graphics g)
+        {
+            g.TranslateTransform(position.X, position.Y);
+            g.FillEllipse(new SolidBrush(Color.FromArgb(128, 128, 128, 128)),
+                new RectangleF(-32, -32, 64, 64));
+            g.ResetTransform();
+        }
+    }
+}
