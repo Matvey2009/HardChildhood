c0de20521700e4265962671bd067f8788b8d834a
Matvey2009 <matvey2009ua@gmail.com>
2020-06-30 21:32:12 +0300

    Create Aobject.cs

---
+++ C#/Танк/Structure/Aobject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace Танк
+{
+    abstract class Aobject
+    {
+        public Color color;     //Цвет команды
+        public PointF position;//Местоподожение
+        public PointF target; //Цель
+        public float vector; //Угол повророта корпуса
+        public float speed; //Скорость
+
+        //Расчёт позиции
+        public PointF Position()
+        {
+            position.X += speed * (float)Math.Cos(vector);
+            position.Y += speed * (float)Math.Sin(vector);
+
+            return position;
+        }
+
+        //Расчёт растаяния между точками (float)
+        public float Delta(PointF point1, PointF point2)
+        {
+            float catetX = point1.X - point2.X;
+            float catetY = point1.Y - point2.Y;
+            return (float)Math.Sqrt(catetX * catetX + catetY * catetY);
+        }
+    }
+}
