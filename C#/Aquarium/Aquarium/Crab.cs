1f172fe6771cec8f2eab548e15b06e860200bfae
Matvey2009 <matvey2009ua@gmail.com>
2020-08-04 21:12:12 +0300

    Create Crab.cs

---
+++ C#/Aquarium/Aquarium/Crab.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Aquarium
+{
+    class Crab : AFish, IFish
+    {
+        public Crab()
+        {
+            bitmap = new Bitmap(Properties.Resources.Lobster);
+            speed = 1.0f;
+        }
+
+        public void DrawFish(Graphics g)
+        {
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
