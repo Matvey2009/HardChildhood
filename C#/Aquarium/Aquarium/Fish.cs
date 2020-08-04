20534dd6a2fba5b7059369d84236366cb7c2499c
Matvey2009 <matvey2009ua@gmail.com>
2020-08-04 21:12:10 +0300

    Create Fish.cs

---
+++ C#/Aquarium/Aquarium/Fish.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Aquarium
+{
+    class Fish : AFish, IFish
+    {
+        public Fish()
+        {
+            bitmap = new Bitmap(Properties.Resources.Fish);
+            speed = 1.5f;
+        }
+
+        public void DrawFish(Graphics g)
+        {
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
