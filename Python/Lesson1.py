3dea3d75a9cba1a8e3442c2c073916e102c5025f
Matvey2009 <matvey2009ua@gmail.com>
2020-08-04 21:11:27 +0300

    Create Lesson1.py

---
+++ Python/Lesson1.py
@@ -0,0 +1,17 @@
+# Урок №1
+print("Hello Word\n" + '\tМатвей')
+print(1+2+3)
+
+x = 20
+print(x)
+
+if x > 0:
+    print("Число =" + str(x))
+x = "выаяф"
+print("Число = " + x)
+
+input()
+
+# Запуск с CMD D:\Documents\Programs\HardChildhood\Python> "C:\Program Files (x86)\Python38-32\python.exe" Leson1.py
+
+# Запуск с патчем в CMD D:\Documents\Programs\HardChildhood\Python>Leson1.py
