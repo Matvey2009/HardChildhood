3dbbf1b6cd7373baec5765af2c0f4e246d87dce9
Matvey2009 <matvey2009ua@gmail.com>
2020-08-04 21:12:08 +0300

    Create Form1.cs

---
+++ C#/Aquarium/Aquarium/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Aquarium
+{
+    public partial class Form1 : Form
+    {
+        public static Size window;
+        public Graphics g;
+        private Fishes fishes;
+
+        //Окно приложения
+        public Form1()
+        {
+            window = ClientSize;
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                     ControlStyles.AllPaintingInWmPaint |
+                     ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        //Таймер
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            fishes.DrawListFish(g);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fishes = new Fishes();
+        }
+    }
+}
