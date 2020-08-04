25000ca446361342ec2e8fc2abe8318a05f02044
Matvey2009 <matvey2009ua@gmail.com>
2020-08-04 21:11:46 +0300

    Create Bubble.cs

---
+++ C#/Aquarium/Aquarium/Resources/Bubble.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Aquarium
+{
+    class Bubble : AFish, IFish
+    {
+        public Bubble()
+        {
+            bitmap = new Bitmap(Properties.Resources.Bubble);
+            speed = 0.5f;
+        }
+
+        public void DrawFish(Graphics g)
+        {
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
