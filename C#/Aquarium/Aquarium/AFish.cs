0a3d4a6650a29b2944ef8f5e6e9a5ff40ba09cb5
Matvey2009 <matvey2009ua@gmail.com>
2020-08-04 21:12:20 +0300

    Create AFish.cs

---
+++ C#/Aquarium/Aquarium/AFish.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+
+namespace Aquarium
+{
+    abstract class AFish
+    {
+        public PointF position = new PointF();
+        public PointF target = new PointF();
+        public Bitmap bitmap;
+        private Random random = new Random();
+        public float speed;
+
+        //Рассчёт позиции
+        public PointF Position()
+        {
+            if (Math.Abs(position.X - target.X) < speed*2 &&
+                Math.Abs(position.Y - target.Y) < speed*2)
+                Target();
+
+            if (target.X > position.X)
+                position.X += speed;
+            else
+                position.X -= speed;
+
+            if (target.Y > position.Y)
+                position.Y += speed;
+            else
+                position.Y -= speed;
+
+            return position;
+        }
+
+        //Рассчёт цели
+        public PointF Target()
+        {
+            target.X = random.Next(0, 1800);
+            target.Y = random.Next(0, 1080);
+
+            return target;
+        }   
+    
+    }
+}
