a29d720dbe62c4d9e5935473c0538ebcaccf2bfd
Matvey2009 <matvey2009ua@gmail.com>
2020-08-12 22:01:20 +0300

    Update Определение високосного года.py

--- Python/Task/Определение високосного года.py
+++ Python/Task/Определение високосного года.py
@@ -5,6 +5,15 @@ import os
 os.system("mode con cols=40 lines=105")
 
 while True:
+    print("      _ _             _ _      ")
+    print("     |   |           |   |     ") 
+    print("     |_ _|           |_ _|     ") 
+    print("                               ") 
+    print("  _ _                     _ _  ") 
+    print(" |   |_ _ _ _ _ _ _ _ _ _|   | ") 
+    print(" |_                         _| ") 
+    print("   | _ _ _ _ _ _ _ _ _ _ _ |   ") 
+    print("                               ") 
     x = int(input("Ведите год\n"))
     os.system("cls||clear")
     if x % 4 == 0 and x % 100 != 0 or x % 400 == 0:
