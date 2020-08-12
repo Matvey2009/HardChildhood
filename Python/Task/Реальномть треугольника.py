54f0af12a790681fa985da40730b4eb5c122d34b
Matvey2009 <matvey2009ua@gmail.com>
2020-08-12 22:01:15 +0300

    Update Реальномть треугольника.py

--- Python/Task/Реальномть треугольника.py
+++ Python/Task/Реальномть треугольника.py
@@ -1,8 +1,18 @@
 #
 while True:
+    print("      _ _             _ _      ")
+    print("     |   |           |   |     ") 
+    print("     |_ _|           |_ _|     ") 
+    print("                               ") 
+    print("  _ _                     _ _  ") 
+    print(" |   |_ _ _ _ _ _ _ _ _ _|   | ") 
+    print(" |_                         _| ") 
+    print("   | _ _ _ _ _ _ _ _ _ _ _ |   ") 
+    print("                               ") 
     a = input("Ведите первое число\n")
     b = input("Ведите второе число\n")
     c = input("Ведите третие число\n")
+    os.system("cls||clear")
     if a + b < c or c + a < b or b + c < a:
         print("Эти числа подходят для треугольника -")
     else:
