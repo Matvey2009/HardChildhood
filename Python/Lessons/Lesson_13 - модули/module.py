eaf57d59c93dd41d9461dc910eb2fd3ecdb5655c
Matvey2009 <matvey2009ua@gmail.com>
2020-08-25 20:30:11 +0300

    Create module.py

---
+++ Python/Lessons/Lesson_13 - модули/module.py
@@ -0,0 +1,14 @@
+# вспомогательный файл к уроку № 13 по Python - ООП
+
+print("Тест импорта")
+
+mmm = "данные"
+
+def plus(x, y):
+    return print(x, "плюс", y, "равно", x + y)
+
+def minus(x, y):
+    return print(x, "минус", y, "равно", x - y)
+    
+# if __name__ == "__main__":
+#     ??????????
