d3b3b02c2f7b37c292e93a77740e7972f7a84ed9
Matvey2009 <matvey2009ua@gmail.com>
2020-08-25 20:30:12 +0300

    Create Main.py

---
+++ Python/Lessons/Lesson_13 - модули/Main.py
@@ -0,0 +1,31 @@
+# Урок № 13 по Python - ООП
+
+#import module
+#import module as pm
+#from module import plus
+#from module import plus, minus
+from module import plus, minus as m
+
+#print("импорт данных", module.mmm)
+
+#module.plus(3, 2)
+
+#module.minus(3, 2)
+
+#pm.plus(3, 2)
+
+#pm.minus(3, 2)
+
+plus(3, 2)
+
+#minus(3, 2)
+
+m(3, 2)
+
+#from random import randrange
+from random import randrange as r
+
+#print("рандомное число", randrange(10))
+print("рандомное число", r(10))
+
+input()
