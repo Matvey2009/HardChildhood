9d300f3086435be912550ad3001a7c7dd39b5a95
Matvey2009 <matvey2009ua@gmail.com>
2020-09-05 20:59:12 +0300

    Update Олимп3.py

--- Python/Олимп/Олимп3.py
+++ Python/Олимп/Олимп3.py
@@ -7,8 +7,10 @@ while c < x:
     c += 1
 for i in kur:
     k = int(i)
-    k = (k-100)//7
-    print(k)
+    if k <= 100 or (k-100)%7 != 0:
+        print(-1)
+    else:
+        print((k-100)//7)
 
 # Для локального запуска
 input()
