deeb42f3d36f54779f5eece54f14922813526687
Matvey2009 <matvey2009ua@gmail.com>
2020-09-13 17:05:37 +0300

    Update Олимп4.py

--- Python/Олимп/Олимп4.py
+++ Python/Олимп/Олимп4.py
@@ -1,14 +1,20 @@
 #Ввод
-N = input("Диапозон номеров")
-M = input("Количество номеров")
-arr = input("Вводим масив")
+N = 10 #input("Диапозон номеров")
+M = 7 #input("Количество номеров")
+arr = "1 3 6 4 3 2 9" #input("Вводим масив")
+arr = arr.split()
+setArr = set(arr)
 
 #Решение
-maxn = 0
 sumn = 0
-
+for i in setArr:
+    x = arr.count(i)
+    if x > sumn:
+        maxn = i
+        sumn = x
+    elif x == sumn and int(maxn) > int(i):
+        maxn = i
+    
 #Вывод
 print(maxn, sumn)
-
-# Для локального запуска
 input()
