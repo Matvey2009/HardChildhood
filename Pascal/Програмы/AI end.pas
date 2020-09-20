fb044ec01ad0c34f677b0e67063304af969895d6
Matvey2009 <matvey2009ua@gmail.com>
2020-09-20 21:35:45 +0300

    Create AI end.pas

---
+++ Pascal/Програмы/AI end.pas
@@ -0,0 +1,26 @@
+﻿Program AI;
+label m;
+var a, b, x, y, i :integer;
+begin
+  a := 0;
+  b := 1000;
+  writeln('Задумайте число от 1 до 1000');
+  m:
+ x :=  (b - a) div 2 + a;
+  writeln('Это число ', x,'? есле больше 1, если меньше то 0, а есле угадали то Любое другое');
+i :=i+1;
+  read (y);
+  if y = 0 then
+    begin
+    b := x;
+    goto m;
+    end;
+  if y = 1 then
+    begin
+    a := x;
+    goto m;
+    end
+   else
+writeln('угадал с попытки ', i);
+end.
+
