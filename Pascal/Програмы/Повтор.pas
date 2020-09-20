610f6b2f8d59549bd8e1b77c5d198bbfdabbfd61
Matvey2009 <matvey2009ua@gmail.com>
2020-09-20 21:35:23 +0300

    Create Повтор.pas

---
+++ Pascal/Програмы/Повтор.pas
@@ -0,0 +1,18 @@
+﻿Program remove;
+
+var x:integer;
+
+Begin
+  write('Ведите число');
+  read(x);
+  if (x < 100) and (x > 0)
+  then writeln('> 100')
+  else writeln('< 100'); 
+  writeln(10 div 3);
+  writeln(10 mod 3);
+  while x > 0 do 
+  begin
+    x := x-1;
+    writeln(x);
+  end;
+End.
