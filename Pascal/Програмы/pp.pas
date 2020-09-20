90ebd6ae12a48493c520dbb46d06ac593390dcf7
Matvey2009 <matvey2009ua@gmail.com>
2020-09-20 21:35:26 +0300

    Create pp.pas

---
+++ Pascal/Програмы/pp.pas
@@ -0,0 +1,30 @@
+﻿Program pp; 
+const M = 100;
+var r:integer; arr : array [1..M] of integer;
+
+
+procedure print;
+begin;
+for var i := 1 to M do write  (' ', arr[i]);
+end;
+
+Begin
+for var i := 1 to M do arr[i] := random (0, 999);
+print;
+writeln;
+writeln;
+
+//Сортировка
+for var i := 1 to M-1 do
+  begin  
+    if arr[i] > arr[i+1] then
+      begin  
+        r := arr[i];
+        arr[i] := arr[i+1];
+        arr[i+1] := arr[i];
+      end;
+  end;
+
+
+print;
+End.
