4a902edabb731349006a7da087866384406cebf9
Matvey2009 <matvey2009ua@gmail.com>
2020-10-25 20:53:42 +0300

    Delete Task_2.pas

--- Pascal/Олимп/Task_2.pas
+++
@@ -1,8 +0,0 @@
-﻿Program Task_2;
-var A, B, C:integer;
-Begin
-  read(A, B, C);
-  if (A < B) and (A < C) then writeln(B + C);
-  if (B < A) and (B < C) then writeln(A + C);
-  if (C < A) and (C < B) then writeln(A + B);
-end.
