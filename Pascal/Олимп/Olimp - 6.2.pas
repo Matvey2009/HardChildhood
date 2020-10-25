6bcc458552231fef4bb997965deb92087ae8d231
Matvey2009 <matvey2009ua@gmail.com>
2020-10-25 21:32:29 +0300

    Create Olimp - 6.2.pas

---
+++ Pascal/Олимп/Olimp - 6.2.pas
@@ -0,0 +1,26 @@
+﻿program p;
+
+var
+  i, R, X, L, Temp: integer;
+  arr: array of integer;
+
+begin
+  read(R);
+  L := 0;
+  Temp := R;
+  while Temp > 0 do
+  begin
+    L := L + 1;
+    Temp := Temp div 10;
+  end;
+  setLength(arr, L);
+  
+  for i := 0 to L-1 do
+  begin
+    arr[i] := R mod 10;
+    R := R 
+    div 10;
+  end;
+  
+  write(arr);
+end.
