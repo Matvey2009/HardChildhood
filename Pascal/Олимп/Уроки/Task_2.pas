ccc4b4abb6f910c64e7ecf3dc9efe62f59794098
Matvey2009 <matvey2009ua@gmail.com>
2020-10-25 20:53:10 +0300

    Create Task_2.pas

---
+++ Pascal/Олимп/Уроки/Task_2.pas
@@ -0,0 +1,8 @@
+﻿Program Task_2;
+var A, B, C:integer;
+Begin
+  read(A, B, C);
+  if (A < B) and (A < C) then writeln(B + C);
+  if (B < A) and (B < C) then writeln(A + C);
+  if (C < A) and (C < B) then writeln(A + B);
+end.
