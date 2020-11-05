7aa343129c72ae72d988cc619c307308a8191108
Matvey2009 <matvey2009ua@gmail.com>
2020-11-05 20:58:43 +0300

    Create Olimp - 11.2.pas

---
+++ Pascal/Олимп/Olimp - 11.2.pas
@@ -0,0 +1,15 @@
+﻿program p;
+var i,  n, v:integer;
+ arr: array of integer;
+begin
+  read(n);
+  setLength(arr, n);
+  
+  for i := 0 to n-1 do
+  begin
+    read(arr[i]);
+  if arr[i] = 0 then writeln(2)
+  else if arr[i] = 1 then writeln(0)
+  else if arr[i] = 2 then writeln(1)
+  end;
+end. 
