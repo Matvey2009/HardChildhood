957ca528594e51c31c2adecadd3a881168f84bf9
Matvey2009 <matvey2009ua@gmail.com>
2021-04-10 21:37:47 +0300

    Relocation

--- Python/RATORI/modules/Game.py
+++ Python/RATORI/modules/Game.py
@@ -1,6 +1,5 @@
 import pygame as pg
 from modules.ground.Ground import Ground
-from modules.unit.Unit import Unit
 from modules.Interface.Interface import Interface
 from modules.unit.Hero import Hero
 
@@ -11,33 +10,46 @@ class Game(object):
         """ Игра """
         self.size = size
         self.ground = Ground()
-        self.unit = Unit()
         self.interface = Interface(size)
         self.hero = Hero(size)
-        self.unit.rect.center = self.positon(size)
         self.hero.rect.center = self.positon(size)
+        self.turn = 'stop'
 
     def update(self, e):
         """ Обнавление """
         size = pg.display.get_window_size()
         if self.size != size:
             self.size = size
-            self.unit.rect.center = self.positon(size)
             self.hero.rect.center = self.positon(size)
-        if e.type == pg.KEYDOWN and e.key == pg.K_UP:
-            self.unit.rect.y -= 3
-        if e.type == pg.KEYDOWN and e.key == pg.K_DOWN:
-            self.unit.rect.y += 3
-        if e.type == pg.KEYDOWN and e.key == pg.K_LEFT:
-            self.unit.rect.x -= 3
-        if e.type == pg.KEYDOWN and e.key == pg.K_RIGHT:
-            self.unit.rect.x += 3
+
+        # Смисок кликов клавиатуры
+        keys = pg.key.get_pressed()
+
+        if (keys[pg.K_RIGHT] and keys[pg.K_DOWN]):
+            self.turn = 'right_down'
+        elif (keys[pg.K_LEFT] and keys[pg.K_DOWN]):
+            self.turn = 'left_down'
+        elif (keys[pg.K_LEFT] and keys[pg.K_UP]):
+            self.turn = 'left_up'
+        elif (keys[pg.K_RIGHT] and keys[pg.K_UP]):
+            self.turn = 'right_up'
+        elif (keys[pg.K_DOWN]):
+            self.turn = 'down'
+        elif (keys[pg.K_LEFT]):
+            self.turn = 'left'
+        elif (keys[pg.K_RIGHT]):
+            self.turn = 'right'
+        elif (keys[pg.K_UP]):
+            self.turn = 'up'
+        else:
+            self.turn = 'stop'
+
+        self.hero.update(self.turn)
         self.interface.update()
 
     def draw(self, g):
         """ Отрисовка """
         self.ground.draw(g)
-        # self.unit.draw(g)
         self.interface.draw(g)
         self.hero.draw(g)
 
--- Python/RATORI/modules/unit/Hero.py
+++ Python/RATORI/modules/unit/Hero.py
@@ -9,29 +9,64 @@ class Hero(object):
     def __init__(self, size):
         """ Конструктор """
         self.rate = self._rate_
-        self.tile_atlas = []
-        self.tile_atlas = self.filling()
+        self.title_atlas = []
+        self.title_atlas = self.filling()
         self.rect = pg.Rect(0, 0, self.rate, self.rate)
+        self.speed = 0
+        self.row = 0
+        self.col = 0
+        self.step = 0
+        self.image = self.title_atlas[self.row][self.col]
 
-    def update(self):
+
+    def update(self, turn):
         """ Обнавление """
-        pass
+        if turn == 'stop':
+            self.image = self.title_atlas[0][0]
+        else:
+            if turn == 'right_down':
+                self.col = 1
+            elif turn == 'left_down':
+                self.col = 7
+            elif turn == 'left_up':
+                self.col = 5
+            elif turn == 'right_up':
+                self.col = 3
+            elif turn == 'down':
+                self.col = 0
+            elif turn == 'left':
+                self.col = 6
+            elif turn == 'up':
+                self.col = 4
+            elif turn == 'right':
+                self.col = 2
+
+        self.select()
+
 
     def draw(self, g):
         """ Отрисовка """
-        image = self.tile_atlas[4][7]
-        g.blit(image, self.rect)
+        self.image = self.title_atlas[self.row][self.col]
+        g.blit(self.image, self.rect)
+
+    def select(self):
+        self.speed += 1
+        if self.speed >= 10:
+            self.row += 1
+            self.speed = 0
+            if self.row >= 4:
+                self.row = 1
 
     def filling(self):
         """ Зополняем Atlas таеломи """
         atlas = self._atlas_
         size = (self.rate, self.rate)
         for row in range(atlas.get_height() // self.rate):
-            self.tile_atlas.append([])
+            self.title_atlas.append([])
             for col in range(atlas.get_width() // self.rate):
                 rect = (col * self.rate, row * self.rate)
                 image = atlas.subsurface(rect, size)
                 image = pg.transform.scale(image, (self.rate * 2, self.rate * 2))
-                self.tile_atlas[row].append(image)
+                self.title_atlas[row].append(image)
 
-        return self.tile_atlas
+        return self.title_atlas
