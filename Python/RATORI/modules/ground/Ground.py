b766640c96b2b4db2aeb86a00825a501e05b0d0e
Matvey2009 <matvey2009ua@gmail.com>
2021-05-10 20:59:19 +0300

    Maping upp

--- Python/RATORI/modules/ground/Ground.py
+++ Python/RATORI/modules/ground/Ground.py
@@ -18,7 +18,7 @@ class Ground(object):
 
     def update(self, size, turn):
         """ Обнавление """
-        scrole_line = 3
+        scrole_line = 10
         scrole = round(scrole_line / 1.4)
 
         if turn == "right_down":
