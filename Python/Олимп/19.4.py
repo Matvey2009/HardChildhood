e6e7d2925e2da1a50048d724a0f7f82a5ec24cfd
Matvey2009 <matvey2009ua@gmail.com>
2021-08-06 20:12:07 +0300

    19.4

--- Python/Олимп/19.4.py
+++ Python/Олимп/19.4.py
@@ -1,9 +1,18 @@
 N = int(input())
 
-arr = []
+arr1 = []
+arr2 = []
+
 for i in range(N):
     string = input()
-    string = list(string)
-    arr.append(string)
-
-print(arr)
+    for j in range(N):
+        if (j + i) % 2 == 0:
+            arr1.append(string[j])
+        else:
+            arr2.append(string[j])
+            
+            
+            
+        
+print(arr1)
+print(arr2)
