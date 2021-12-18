836d777bd041ecb739abb67fcbf3c9e769d7aa8e
Matvey2009 <matvey2009ua@gmail.com>
2021-12-18 18:15:27 +0300

    ASM

---
+++ C++/SPP+APM_1/SPP+APM_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int x;
+    int y;
+    int res;
+
+    cin >> x;
+    cin >> y;
+
+    __asm
+    {
+        mov eax, x
+        mov ebx, y
+        add eax, ebx
+        mov res, eax
+    }
+
+    cout << res;
+
+    //Оптимизацыя повторного использования данных
+    cin >> x;
+
+    __asm 
+    {
+        mov eax, x
+        //mov ebx, y
+        add eax, ebx
+        mov res, eax
+    }
+
+    cout << res;
+}
