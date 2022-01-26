66cac44c0377e71779df3c674418f46c9940ad24
Matvey2009 <matvey2009ua@gmail.com>
2022-01-26 21:03:14 +0300

    Create Урок 2.lua

---
+++ Lua/Урок 2.lua
@@ -0,0 +1,27 @@
+--26.01.2022--
+os.setlocale('rus_rus.1251')
+
+x = io.read('*n')
+
+if x == 0 then
+	print('727')
+elseif x > 0 and (x < 5 or x == 5) then
+	print('1-5')
+else
+	print('6-...')
+end
+
+function summ(a, b)
+	local c = a + b
+	return c
+end
+print(summ(700, 27))
+print(c)
+
+multi = function(a, b)
+	local c = a * b
+	return c
+end
+print(multi(5, 6))
+
+x = io.read('*n')
