54d19ed23320c60b5b8ad1af09186511c65c1b6f
Matvey2009 <matvey2009ua@gmail.com>
2022-01-27 21:24:38 +0300

    Update Урок 1.lua

--- Lua/Урок 1.lua
+++ Lua/Урок 1.lua
@@ -7,7 +7,7 @@
 os.setlocale('rus_rus.1251')
 
 io.write('Привет')
-print("Привет МИР")
+print("Привет МИР")--print Преобразовывает в стринг а io.write просто выводит--
 print([[
 Привет
 Мир...
@@ -22,11 +22,14 @@ print(type(nil))
 
 --Отынтовка в число--
 y = tonumber('5') --Строки всегда конкстанты--
+y = tostring('5') --Преобразовывает число в строку--
 print(y)
 x, y = y, x
 print(x, y)
 print(x..y)
 a, b, c = 7, 2, 7
 print(a..b..c)
+
+print(5^2)--Степень--
 
 x = io.read('*n')
