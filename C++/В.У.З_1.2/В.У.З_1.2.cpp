c9b2dadf0a57d146c1403800bc8ff3f41ebfc80c
Matvey2009 <matvey2009ua@gmail.com>
2022-07-19 20:35:02 +0300

    BYZ12

---
+++ C++/В.У.З_1.2/В.У.З_1.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int n, s;
+	cout << "Введите n" << endl;
+	cin >> n;
+	s = 1;
+	for (int i = 1; i < n+1; i++) {
+		s *= i;
+	}
+	cout << s;
+
+	system("pause>nul");
+	return 0;
+}
---
+++ Python/ВУЗ/В.У.З_12.py
@@ -0,0 +1,7 @@
+print("Введите n")
+n = int(input())
+s = 1
+for i in range(1, n+1):
+	s *= i
+print(s)
+input()
