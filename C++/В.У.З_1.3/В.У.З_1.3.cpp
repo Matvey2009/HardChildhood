167db080aec3d678bd1f14693ed9292e8e724ca2
Matvey2009 <matvey2009ua@gmail.com>
2022-08-02 21:30:58 +0300

    Решение задач

---
+++ C++/В.У.З_1.3/В.У.З_1.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int n, i;
+	string s;
+	cout << "Введите число" << endl;
+	cin >> n;
+	s = "Простое";
+	if (n < 2 || n > 10000000000)
+		cout << "Введины не коректные данные";
+	else {
+		for (i = 2; i < n - 1; i++) {
+			if (n % i == 0) {
+				s = "Составное";
+				break;
+			}
+		}
+		cout << "Ответ: " << s;
+	}
+
+	system("pause>nul");
+	return 0;
+}
---
+++ C++/В.У.З_1.4/В.У.З_1.4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int n, s, j;
+	j = 1;
+	s = 0;
+	cout << "Задайте число" << endl;
+	cin >> n;
+	if (n < 0 || n == 10000000000000000)
+		cout << "Введите корректные данные";
+	else
+	{
+		while (n >= j) {
+			j *= 2;
+			s++;
+		}
+		cout << s;
+	}
+	system("pause>nul");
+	return 0;
+}
