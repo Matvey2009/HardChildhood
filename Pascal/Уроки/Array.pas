2a7c664a031b1fe62700f53eaf6ffe8126ede203
Matvey2009 <matvey2009ua@gmail.com>
2022-10-03 20:56:41 +0300

    Create Array.pas

---
+++ Pascal/Уроки/Array.pas
@@ -0,0 +1,13 @@
+﻿program урок;
+var statArr: array[0..5] of integer;
+       dynArr: array of integer;
+       x: word;
+begin
+  statArr[0] := 111;
+  writeln(statArr[0]);
+  
+  read(x);
+  setLength(dynArr, x);
+  dynArr[0] := 222;
+  writeln(dynArr);
+end.
