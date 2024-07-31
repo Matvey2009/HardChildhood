126b455011e8d7d5c77ab54c191d690e8ffd26bd
Matvey <55892080+Matvey2009@users.noreply.github.com>
2024-07-31 16:14:37 +0300

    Update app.py

--- Python/RS/app.py
+++ Python/RS/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return "Hello World"
-python app.py
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
