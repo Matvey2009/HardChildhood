d6c049cf786bfbad7d416a9ef41e309822591e36
Matvey <55892080+Matvey2009@users.noreply.github.com>
2024-08-02 13:13:32 +0300

    Layout

--- Python/RS/app.py
+++ Python/RS/app.py
@@ -6,11 +6,34 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    data = {
+        'title': "Главная странница",
+        'contener': "Введите любое число от 0 до 100"
+    }
+    return render_template('index.html', data=data)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    data = {
+        'title': "About",
+        'contener': "About contener"
+    }
+    return render_template('about.html', data=data)
+
+@app.route('/registration')
+def registration():
+    data = {
+        'title': "Регестрация"
+    }
+    return render_template('registration.html', data=data)
+
+@app.route('/table')
+def table():
+    data = {
+        'title': "Таблица"
+    }
+    return render_template('table.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+#registration
